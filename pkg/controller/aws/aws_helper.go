d9c1a14e9a87f6c57882571632dc7be6a0344c01
Rael Garcia <rael@redhat.com>
2020-05-29 10:38:08 +0200

    Instance a client to interact with the AWS API

---
+++ pkg/controller/aws/aws_helper.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi"
+)
+
+// AWSClient is the struct implementing the lbprovider interface
+type AWSClient struct {
+	elbv2  *elbv2.ELBV2
+	rgtapi *resourcegroupstaggingapi.ResourceGroupsTaggingAPI
+}
+
+// newAWSClient obtains an AWS session and initiates the needed AWS clients.
+func newAWSClient(id string, secret string, region string) (*AWSClient, error) {
+
+	// Get AWS config
+	awsConfig := &aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(id, secret, ""),
+	}
+
+	// Initialize an AWS session
+	awsConfig = awsConfig.WithCredentialsChainVerboseErrors(true)
+	sess, err := session.NewSession(awsConfig)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to initialize AWS session: %v", err)
+	}
+
+	// Return AWS clients for ELBV2 and ResourceGroupsTaggingAPI
+	return &AWSClient{
+		elbv2:  elbv2.New(sess),
+		rgtapi: resourcegroupstaggingapi.New(sess),
+	}, nil
+}
